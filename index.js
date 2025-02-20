0c6ba094be324e3f70f42b00b59ea15c8fe64503
Nick Roman <nick@heronschool.com>
2025-02-20 21:07:01 +0000

    last day updates with reply

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ import cors from "cors";
 import { sendTextToPhone } from "./twilio.js";
 import dotenv from "dotenv";
 dotenv.config();
+import bodyParser from "body-parser";
+import twilio from "twilio";
 
 const app = express();
 const PORT = 4000;
 
+let replyMessage;
+
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
   console.log(`App is now running on ${PORT}`);
@@ -29,4 +34,19 @@ app.post("/send", async (req, res) => {
   } catch (error) {
     res.json("Failure, You Are");
   }
+});
+
+app.post("/sms", (req, res) => {
+  const { MessagingResponse } = twilio.twiml;
+  const twiml = new MessagingResponse();
+
+  console.log({ returnText: req.body.Body });
+
+  replyMessage = req.body.Body;
+
+  res.type("text/sml").send(twiml.toString());
+});
+
+app.get("/replyMessages", (req, res) => {
+  res.json(replyMessage);
 });
